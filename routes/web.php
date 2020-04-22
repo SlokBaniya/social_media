17a45580b7183d2f4536909c705bb4c064ca0f4e
SlokBaniya <slokbaniya1@gmail.com>
2020-04-22 09:54:22 +0545

    Create User Login and Registration

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-    return view('user/home');
+    return view('welcome');
 });
+
+// Route::get('/', function () {
+//     return view('user/home');
+// });
 
 Route::get('/user/home','UserController@user_home')->name('user.home');
 Route::get('/user/profile','UserController@user_profile')->name('user.profile');
@@ -27,3 +27,7 @@ Route::get('/user/search','UserController@user_search')->name('user.search');
 Route::get('/user/upload','UserController@user_upload')->name('user.upload');
 Route::get('/user/stories','UserController@user_stories')->name('user.stories');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
